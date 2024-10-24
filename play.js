ae0d4f02cf361348765b6d6c07d1ba13e66f2729
MPNose <mpnoseworthy89@gmail.com>
2024-10-23 19:22:38 -0700

    setupInput function, and handleUserInput function created

--- play.js
+++ play.js
@@ -6,4 +6,22 @@ const net = require("net");
 
 
 console.log("Connecting ...");
+
+const setupInput = function() {
+  const stdin = process.stdin; //create variable to hold stdin object, so we don't have to type it multiple times
+  stdin.setRawMode(true); //Raw Mode allows us to listen for individual key presses instead of the user to press enter
+  stdin.setEncoding("utf8"); //set encoding so that we can read the text data that is input
+  stdin.resume(); //resume stdin so the program can listen for input
+  stdin.on("data", handleUserInput);//callback function waiting for key input from control c
+  return stdin; //return the stdin object so we can use it elsewhere in the program
+};
+const handleUserInput = function (key) {
+  if (key === "\u0003") {//code representing control c 
+    process.exit();
+  };
+};
+
+
+
 connect();
+setupInput();

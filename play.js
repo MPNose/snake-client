0aff86f84be51fd2125a9ca9b81f142e47aba7b4
MPNose <mpnoseworthy89@gmail.com>
2024-10-23 19:43:26 -0700

    refactored play.js into input.js file

--- play.js
+++ play.js
@@ -1,4 +1,5 @@
 const connect = require("./client");
+const setupInput = require('./input');
 const net = require("net");
 // use net library to create a connection
 // establishes a connection with the game server
@@ -7,19 +8,6 @@ const net = require("net");
 
 console.log("Connecting ...");
 
-const setupInput = function() {
-  const stdin = process.stdin; //create variable to hold stdin object, so we don't have to type it multiple times
-  stdin.setRawMode(true); //Raw Mode allows us to listen for individual key presses instead of the user to press enter
-  stdin.setEncoding("utf8"); //set encoding so that we can read the text data that is input
-  stdin.resume(); //resume stdin so the program can listen for input
-  stdin.on("data", handleUserInput);//callback function waiting for key input from control c
-  return stdin; //return the stdin object so we can use it elsewhere in the program
-};
-const handleUserInput = function (key) {
-  if (key === "\u0003") {//code representing control c 
-    process.exit();
-  };
-};
 
 
 
